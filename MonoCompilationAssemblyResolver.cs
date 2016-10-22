630615f474cac3d25d6a1e644c20ba6f54210dce
Julien Batonnet <julien.batonnet@gmail.com>
2016-10-23 00:22:46 +0200

    Initial commit. Ugly method, but this allows to start ASP.NET Core project with Mono.

---
+++ MonoCompilationAssemblyResolver.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.DependencyModel;
+using Microsoft.Extensions.DependencyModel.Resolution;
+
+namespace Microsoft.AspNetCore.Mono
+{
+    public class MonoCompilationAssemblyResolver : ICompilationAssemblyResolver
+    {
+        public ICompilationAssemblyResolver Resolver { get; }
+
+        public MonoCompilationAssemblyResolver(ICompilationAssemblyResolver resolver)
+        {
+            Resolver = resolver;
+        }
+
+        public bool TryResolveAssemblyPaths(CompilationLibrary library, List<string> assemblies)
+        {
+            string[] searchDirectories = new string[]
+            {
+                Environment.CurrentDirectory,
+                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                "/usr/lib/mono/4.5",
+                "/usr/lib/mono/4.5/Facades",
+            };
+
+            foreach (string directory in searchDirectories)
+            {
+                FileInfo fileInfo = new FileInfo(Path.Combine(directory, library.Name + ".dll"));
+                if (fileInfo.Exists)
+                {
+                    assemblies.Add(fileInfo.FullName);
+                    return true;
+                }
+            }
+
+            return Resolver.TryResolveAssemblyPaths(library, assemblies);
+        }
+    }
+}
---
+++ WebHostBuilderExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyModel;
+using Microsoft.Extensions.DependencyModel.Resolution;
+
+namespace Microsoft.AspNetCore.Mono
+{
+    public static class WebHostBuilderExtensions
+    {
+        public static IWebHostBuilder UseMonoCompatibility(this IWebHostBuilder hostBuilder)
+        {
+            // Access default resolver
+            Type compilationLibraryType = typeof(CompilationLibrary);
+            PropertyInfo defaultResolverProperty = compilationLibraryType.GetProperty("DefaultResolver", BindingFlags.NonPublic | BindingFlags.Static);
+
+            CompositeCompilationAssemblyResolver defaultResolver = defaultResolverProperty.GetValue(null) as CompositeCompilationAssemblyResolver;
+
+            // Access resolvers array
+            Type compositeCompilationAssemblyResolverType = typeof(CompositeCompilationAssemblyResolver);
+            FieldInfo resolversField = compositeCompilationAssemblyResolverType.GetField("_resolvers", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            // Replace .NET resolver with a Mono one
+            ICompilationAssemblyResolver[] resolvers = resolversField.GetValue(defaultResolver) as ICompilationAssemblyResolver[];
+            for (int i = 0; i < resolvers.Length; i++)
+            {
+                ReferenceAssemblyPathResolver referenceAssemblyPathResolver = resolvers[i] as ReferenceAssemblyPathResolver;
+
+                if (referenceAssemblyPathResolver != null)
+                    resolvers[i] = new MonoCompilationAssemblyResolver(referenceAssemblyPathResolver);
+            }
+
+            return hostBuilder;
+        }
+    }
+}
